3358059f7f98bbe1b73f61816735d536ca617a24
fachidiot <jerrykim428@gmail.com>
2020-10-13 18:00:39 +0900

    Grid Generator

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [Header("Grid Size")]
+    public int GridSize;
+    [Tooltip("GridObject")]
+    public GameObject GridPrefab;
+
+    private GameObject[,] Grid;
+
+    void Start()
+    {
+        GridGenerate(GridSize);
+    }
+
+    // GridGenerator
+    public void GridGenerate(int _wordCount)
+    {
+        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(GridSize * 200, GridSize * 200);
+        this.Grid = new GameObject[_wordCount,_wordCount];
+        this.GridSize = _wordCount;
+        for (int i = 0; i < this.GridSize; i++)
+            for (int j = 0; j < this.GridSize; j++)
+            {
+                this.Grid[i, j] = Instantiate(GridPrefab, this.gameObject.transform);
+                Grid[i, j].transform.position += new Vector3((i - (GridSize - 1) / 2 + 0.3f) * 0.15f, (j - (GridSize - 2) / 2 - 0.3f) * 0.15f, 0);
+                Debug.Log(Grid[i, j].transform.position);
+            }
+    }
+
+    void SetGrid(int x, int y, bool _close = true)
+    {
+        this.Grid[x, y].SetActive(_close);
+    }
+}
---
+++ Assets/Scripts/WordList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordList
+{
+    private List<Word> m_WordList = new List<Word>();
+
+    private class Word
+    {
+        private string m_Answer;
+        public string Answer { get { return m_Answer; } set { m_Answer = value; } }
+        private string m_Meaning;
+        public string Meaning { get { return m_Meaning; } set { m_Meaning = value; } }
+        private int m_Length;
+
+        public Word(string _answ, string _mean)
+        {
+            this.Answer = _answ;
+            this.Meaning = _mean;
+        }
+
+        public void Print()
+        {
+            Debug.Log(Answer);
+            Debug.Log(Meaning);
+        }
+    }
+}
